52730cfce62d19cef619421ff9f350e402498f96
Sanaz Bahmani <sanazbahmani35@gmail.com>
2023-08-29 12:19:06 +0330

    Bind text

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      greet: "Hello",
+      name: "Sanaz",
+    };
+  },
+};
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <p>{{ greet }} {{ name }}</p>
 </template>
 
 <style scoped>
