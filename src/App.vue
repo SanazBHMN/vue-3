75912cc792c68ac1644995ccbdd2d6dcbefd413c
Sanaz Bahmani <sanazbahmani35@gmail.com>
2023-08-29 15:52:17 +0330

    Bind styles

--- src/App.vue
+++ src/App.vue
@@ -12,6 +12,27 @@ export default {
       status: "danger",
       isPromoted: true,
       isSoldout: true,
+      highlightColor: "orange",
+      headerSize: 50,
+      headerStyleObject: {
+        color: "orange",
+        fontSize: "50px",
+        padding: "20px",
+      },
+      baseStyleObject: {
+        fontSize: "50px",
+        padding: "10px",
+      },
+      successStyleObject: {
+        color: "green",
+        backgroundColor: "lightgreen",
+        border: "1px solid green",
+      },
+      dangerStyleObject: {
+        color: "darkred",
+        backgroundColor: "red",
+        border: "1px solid darkred",
+      },
     };
   },
 };
@@ -40,6 +61,20 @@ export default {
   >
     Object conditional movie
   </h2>
+  <h2
+    v-bind:style="{
+      color: highlightColor,
+      fontSize: headerSize + 'px',
+      padding: '20px',
+    }"
+  >
+    Inline Style
+  </h2>
+  <h2 v-bind:style="headerStyleObject">Object style</h2>
+  <div v-bind:style="[baseStyleObject, successStyleObject]">Success Style</div>
+  <div v-bind:style="[baseStyleObject, successStyleObject, dangerStyleObject]">
+    Danger Style
+  </div>
 </template>
 
 <style scoped>
