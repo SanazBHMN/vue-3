ca67ad0cb9caef83ce81b4beac3f56917ddd0d4d
Sanaz Bahmani <sanazbahmani35@gmail.com>
2023-08-30 14:41:21 +0330

    Conditional Rendering - I

--- src/App.vue
+++ src/App.vue
@@ -3,93 +3,24 @@ export default {
   name: "App",
   data() {
     return {
-      greet: "Hello",
-      name: "Sanaz",
-      location: "<b>Helsinki, Finland</b>",
-      hack: `<a href='#' onclick='alert("You have been hacked!")'>Win a prize!</a>`,
-      headingId: "heading",
-      isDisabled: true,
-      status: "danger",
-      isPromoted: true,
-      isSoldout: true,
-      highlightColor: "orange",
-      headerSize: 50,
-      headerStyleObject: {
-        color: "orange",
-        fontSize: "50px",
-        padding: "20px",
-      },
-      baseStyleObject: {
-        fontSize: "50px",
-        padding: "10px",
-      },
-      successStyleObject: {
-        color: "green",
-        backgroundColor: "lightgreen",
-        border: "1px solid green",
-      },
-      dangerStyleObject: {
-        color: "darkred",
-        backgroundColor: "red",
-        border: "1px solid darkred",
-      },
+      num: "Hi",
+      display: true,
     };
   },
 };
 </script>
 
 <template>
-  <p>{{ greet }} {{ name }}</p>
-  <div v-html="location"></div>
-  <div v-html="hack"></div>
-  <h2 :id="headingId">Heading</h2>
-  <button :disabled="isDisabled">Bind</button>
-  <h2 class="underline">Underlined Text</h2>
-  <h2 class="underline" :class="status">Status</h2>
-  <h2 :class="isPromoted && 'promoted'">Promoted Movie</h2>
-  <h2 :class="isSoldout ? 'sold-out' : 'new'">Soldout? movie</h2>
-  <h2 :class="['new', 'promoted']">Newly promoted movie</h2>
-  <h2 :class="[isPromoted && 'promoted', isSoldout ? 'sold-out' : 'new']">
-    Array conditional movie
-  </h2>
-  <h2
-    :class="{
-      promoted: isPromoted,
-      new: !isSoldout,
-      'sold-out': isSoldout,
-    }"
-  >
-    Object conditional movie
-  </h2>
-  <h2
-    :style="{
-      color: highlightColor,
-      fontSize: headerSize + 'px',
-      padding: '20px',
-    }"
-  >
-    Inline Style
-  </h2>
-  <h2 :style="headerStyleObject">Object style</h2>
-  <div :style="[baseStyleObject, successStyleObject]">Success Style</div>
-  <div :style="[baseStyleObject, successStyleObject, dangerStyleObject]">
-    Danger Style
-  </div>
+  <h2 v-if="num === 0">The number is zero</h2>
+  <h2 v-else-if="num > 0">The number is positive</h2>
+  <h2 v-else-if="num < 0">The number is negative</h2>
+  <h2 v-else>Not a number</h2>
+
+  <template v-if="display">
+    <h2>Sanaz</h2>
+    <h2>Software Developer</h2>
+    <h2>JobGo International Oy</h2>
+  </template>
 </template>
 
-<style scoped>
-.underline {
-  text-decoration: underline;
-}
-.promoted {
-  font-style: italic;
-}
-
-.new {
-  color: olivedrab;
-}
-
-.sold-out {
-  color: red;
-}
-</style>
+<style scoped></style>
