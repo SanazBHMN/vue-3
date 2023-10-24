a382aab85c281f5f209ff5d5c8f97c415fbc38cb
Sanaz Bahmani <sanazbahmani35@gmail.com>
2023-10-24 12:27:30 +0330

    methods

--- src/App.vue
+++ src/App.vue
@@ -1,95 +1,28 @@
+<template>
+  <h2>Add method - {{ add(2, 3, 5) }}</h2>
+  <h2>Add method - {{ add(5, 10, 15) }}</h2>
+  <h2>Multiply method - {{ multiply(10) }}</h2>
+  <h2>Multiply method - {{ multiply(baseValue) }}</h2>
+</template>
+
 <script>
 export default {
   name: "App",
   data() {
     return {
-      greet: "Hello",
-      name: "Sanaz",
-      location: "<b>Helsinki, Finland</b>",
-      hack: `<a href='#' onclick='alert("You have been hacked!")'>Win a prize!</a>`,
-      headingId: "heading",
-      isDisabled: true,
-      status: "danger",
-      isPromoted: true,
-      isSoldout: true,
-      highlightColor: "orange",
-      headerSize: 50,
-      headerStyleObject: {
-        color: "orange",
-        fontSize: "50px",
-        padding: "20px",
-      },
-      baseStyleObject: {
-        fontSize: "50px",
-        padding: "10px",
-      },
-      successStyleObject: {
-        color: "green",
-        backgroundColor: "lightgreen",
-        border: "1px solid green",
-      },
-      dangerStyleObject: {
-        color: "darkred",
-        backgroundColor: "red",
-        border: "1px solid darkred",
-      },
+      baseMultiplier: 5,
+      baseValue: 2,
     };
+  },
+  methods: {
+    add(a, b, c) {
+      return a + b + c;
+    },
+    multiply(num) {
+      return num * this.baseMultiplier;
+    },
   },
 };
 </script>
 
-<template>
-  <p>{{ greet }} {{ name }}</p>
-  <div v-html="location"></div>
-  <div v-html="hack"></div>
-  <h2 :id="headingId">Heading</h2>
-  <button :disabled="isDisabled">Bind</button>
-  <h2 class="underline">Underlined Text</h2>
-  <h2 class="underline" :class="status">Status</h2>
-  <h2 :class="isPromoted && 'promoted'">Promoted Movie</h2>
-  <h2 :class="isSoldout ? 'sold-out' : 'new'">Soldout? movie</h2>
-  <h2 :class="['new', 'promoted']">Newly promoted movie</h2>
-  <h2 :class="[isPromoted && 'promoted', isSoldout ? 'sold-out' : 'new']">
-    Array conditional movie
-  </h2>
-  <h2
-    :class="{
-      promoted: isPromoted,
-      new: !isSoldout,
-      'sold-out': isSoldout,
-    }"
-  >
-    Object conditional movie
-  </h2>
-  <h2
-    :style="{
-      color: highlightColor,
-      fontSize: headerSize + 'px',
-      padding: '20px',
-    }"
-  >
-    Inline Style
-  </h2>
-  <h2 :style="headerStyleObject">Object style</h2>
-  <div :style="[baseStyleObject, successStyleObject]">Success Style</div>
-  <div :style="[baseStyleObject, successStyleObject, dangerStyleObject]">
-    Danger Style
-  </div>
-</template>
-
-<style scoped>
-.underline {
-  text-decoration: underline;
-}
-.promoted {
-  font-style: italic;
-}
-
-.new {
-  color: olivedrab;
-}
-
-.sold-out {
-  color: red;
-}
-</style>
+<style scoped></style>
