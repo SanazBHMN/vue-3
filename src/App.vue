341eaf3617d038ca02f00740a491280e73bb4f35
Sanaz Bahmani <sanazbahmani35@gmail.com>
2023-10-25 12:37:45 +0330

    event handling 1

--- src/App.vue
+++ src/App.vue
@@ -1,95 +1,38 @@
+<template>
+  <h2>{{ name }}</h2>
+  <div>
+    <button v-on:mouseover="name = 'Queena'">Change Name</button>
+  </div>
+
+  <h2>{{ count }}</h2>
+  <p>Inline event handling</p>
+  <button v-on:click="count += 1">Incremant</button>
+  <button v-on:click="count -= 1">Decrement</button>
+
+  <p>Define methods</p>
+  <button v-on:click="increment(1)">Increment 1</button>
+  <button v-on:click="increment(5)">Increment 5</button>
+  <button v-on:click="decrement(1)">Decrement 1</button>
+</template>
+
 <script>
 export default {
   name: "App",
   data() {
     return {
-      greet: "Hello",
       name: "Sanaz",
-      location: "<b>Helsinki, Finland</b>",
-      hack: `<a href='#' onclick='alert("You have been hacked!")'>Win a prize!</a>`,
-      headingId: "heading",
-      isDisabled: true,
-      status: "danger",
-      isPromoted: true,
-      isSoldout: true,
-      highlightColor: "orange",
-      headerSize: 50,
-      headerStyleObject: {
-        color: "orange",
-        fontSize: "50px",
-        padding: "20px",
-      },
-      baseStyleObject: {
-        fontSize: "50px",
-        padding: "10px",
-      },
-      successStyleObject: {
-        color: "green",
-        backgroundColor: "lightgreen",
-        border: "1px solid green",
-      },
-      dangerStyleObject: {
-        color: "darkred",
-        backgroundColor: "red",
-        border: "1px solid darkred",
-      },
+      count: 0,
     };
+  },
+  methods: {
+    increment(num) {
+      this.count += num;
+    },
+    decrement(num) {
+      this.count -= num;
+    },
   },
 };
 </script>
 
-<template>
-  <p>{{ greet }} {{ name }}</p>
-  <div v-html="location"></div>
-  <div v-html="hack"></div>
-  <h2 :id="headingId">Heading</h2>
-  <button :disabled="isDisabled">Bind</button>
-  <h2 class="underline">Underlined Text</h2>
-  <h2 class="underline" :class="status">Status</h2>
-  <h2 :class="isPromoted && 'promoted'">Promoted Movie</h2>
-  <h2 :class="isSoldout ? 'sold-out' : 'new'">Soldout? movie</h2>
-  <h2 :class="['new', 'promoted']">Newly promoted movie</h2>
-  <h2 :class="[isPromoted && 'promoted', isSoldout ? 'sold-out' : 'new']">
-    Array conditional movie
-  </h2>
-  <h2
-    :class="{
-      promoted: isPromoted,
-      new: !isSoldout,
-      'sold-out': isSoldout,
-    }"
-  >
-    Object conditional movie
-  </h2>
-  <h2
-    :style="{
-      color: highlightColor,
-      fontSize: headerSize + 'px',
-      padding: '20px',
-    }"
-  >
-    Inline Style
-  </h2>
-  <h2 :style="headerStyleObject">Object style</h2>
-  <div :style="[baseStyleObject, successStyleObject]">Success Style</div>
-  <div :style="[baseStyleObject, successStyleObject, dangerStyleObject]">
-    Danger Style
-  </div>
-</template>
-
-<style scoped>
-.underline {
-  text-decoration: underline;
-}
-.promoted {
-  font-style: italic;
-}
-
-.new {
-  color: olivedrab;
-}
-
-.sold-out {
-  color: red;
-}
-</style>
+<style scoped></style>
