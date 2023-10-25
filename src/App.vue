bc744e73ac5c5e602f73a2686fc763527006eaa9
Sanaz Bahmani <sanazbahmani35@gmail.com>
2023-10-25 15:12:24 +0330

    form handling 1

--- src/App.vue
+++ src/App.vue
@@ -1,95 +1,90 @@
+<template>
+  <div>
+    <pre>{{ JSON.stringify(formValues, null, 2) }}</pre>
+  </div>
+  <form action="">
+    <div>
+      <label for="name">Name</label>
+      <input type="text" id="name" v-model="formValues.name" />
+    </div>
+
+    <div>
+      <label for="profile">Profile Summary</label>
+      <textarea id="profile" v-model="formValues.profileSummary"></textarea>
+    </div>
+
+    <div>
+      <label for="country">Country</label>
+      <select id="country" v-model="formValues.country">
+        <option value="">Select a country</option>
+        <option value="iran">Iran</option>
+        <option value="finland">Finland</option>
+        <option value="canada">Canada</option>
+      </select>
+    </div>
+
+    <div>
+      <label for="job-location">Job Location</label>
+      <select id="job-location" multiple v-model="formValues.jobLocation">
+        <option value="iran">Iran</option>
+        <option value="finland">Finland</option>
+        <option value="canada">Canada</option>
+      </select>
+    </div>
+  </form>
+</template>
+
 <script>
 export default {
   name: "App",
   data() {
     return {
-      greet: "Hello",
-      name: "Sanaz",
-      location: "<b>Helsinki, Finland</b>",
-      hack: `<a href='#' onclick='alert("You have been hacked!")'>Win a prize!</a>`,
-      headingId: "heading",
-      isDisabled: true,
-      status: "danger",
-      isPromoted: true,
-      isSoldout: true,
-      highlightColor: "orange",
-      headerSize: 50,
-      headerStyleObject: {
-        color: "orange",
-        fontSize: "50px",
-        padding: "20px",
-      },
-      baseStyleObject: {
-        fontSize: "50px",
-        padding: "10px",
-      },
-      successStyleObject: {
-        color: "green",
-        backgroundColor: "lightgreen",
-        border: "1px solid green",
-      },
-      dangerStyleObject: {
-        color: "darkred",
-        backgroundColor: "red",
-        border: "1px solid darkred",
+      formValues: {
+        name: "",
+        profileSummary: "",
+        country: "",
+        jobLocation: [],
       },
     };
   },
 };
 </script>
 
-<template>
-  <p>{{ greet }} {{ name }}</p>
-  <div v-html="location"></div>
-  <div v-html="hack"></div>
-  <h2 :id="headingId">Heading</h2>
-  <button :disabled="isDisabled">Bind</button>
-  <h2 class="underline">Underlined Text</h2>
-  <h2 class="underline" :class="status">Status</h2>
-  <h2 :class="isPromoted && 'promoted'">Promoted Movie</h2>
-  <h2 :class="isSoldout ? 'sold-out' : 'new'">Soldout? movie</h2>
-  <h2 :class="['new', 'promoted']">Newly promoted movie</h2>
-  <h2 :class="[isPromoted && 'promoted', isSoldout ? 'sold-out' : 'new']">
-    Array conditional movie
-  </h2>
-  <h2
-    :class="{
-      promoted: isPromoted,
-      new: !isSoldout,
-      'sold-out': isSoldout,
-    }"
-  >
-    Object conditional movie
-  </h2>
-  <h2
-    :style="{
-      color: highlightColor,
-      fontSize: headerSize + 'px',
-      padding: '20px',
-    }"
-  >
-    Inline Style
-  </h2>
-  <h2 :style="headerStyleObject">Object style</h2>
-  <div :style="[baseStyleObject, successStyleObject]">Success Style</div>
-  <div :style="[baseStyleObject, successStyleObject, dangerStyleObject]">
-    Danger Style
-  </div>
-</template>
-
 <style scoped>
-.underline {
-  text-decoration: underline;
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  /* text-align: center; */
+  color: #2c3e50;
+  margin-top: 60px;
 }
-.promoted {
-  font-style: italic;
-}
-
-.new {
-  color: olivedrab;
-}
-
 .sold-out {
   color: red;
+}
+label {
+  font-weight: bold;
+  display: flex;
+  margin-bottom: 5px;
+}
+input + label {
+  font-weight: bold;
+  display: inline-flex;
+  margin-right: 20px;
+}
+input[type="text"],
+textarea,
+select {
+  display: block;
+  width: 400px;
+  padding: 6px 12px;
+  font-size: 14px;
+  line-height: 1.42857143;
+  color: #555;
+  background-color: #fff;
+  background-image: none;
+  border: 1px solid #ccc;
+  border-radius: 4px;
 }
 </style>
