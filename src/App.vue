7507dd9a49a45b5984be9c72ae68714d1fbf5e4f
Sanaz Bahmani <sanazbahmani35@gmail.com>
2023-10-31 12:52:21 +0330

    Form handling 2

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div>
     <pre>{{ JSON.stringify(formValues, null, 2) }}</pre>
   </div>
-  <form action="">
+  <form @submit="submitForm">
     <div>
       <label for="name">Name</label>
       <input type="text" id="name" v-model="formValues.name" />
@@ -31,6 +31,78 @@
         <option value="canada">Canada</option>
       </select>
     </div>
+
+    <div>
+      <input
+        type="checkbox"
+        id="remoteWork"
+        v-model="formValues.remoteWork"
+        true-value="yes"
+        false-value="no"
+      />
+      <label for="remoteWork">Open to remote work?</label>
+    </div>
+
+    <div>
+      <label>Skill set</label>
+      <input
+        type="checkbox"
+        id="html"
+        value="html"
+        v-model="formValues.skillSet"
+      />
+      <label for="html">HTML</label>
+      <input
+        type="checkbox"
+        id="css"
+        value="css"
+        v-model="formValues.skillSet"
+      />
+      <label for="css">CSS</label>
+      <input
+        type="checkbox"
+        id="javascript"
+        value="javascript"
+        v-model="formValues.skillSet"
+      />
+      <label for="javascript">JavaScript</label>
+    </div>
+
+    <div>
+      <label>Years of Experience</label>
+      <input
+        type="radio"
+        id="0-2"
+        value="0-2"
+        v-model="formValues.yearsOfExperience"
+      />
+      <label for="0-2">0-2</label>
+      <input
+        type="radio"
+        id="3-5"
+        value="3-5"
+        v-model="formValues.yearsOfExperience"
+      />
+      <label for="3-5">3-5</label>
+      <input
+        type="radio"
+        id="6-10"
+        value="6-10"
+        v-model="formValues.yearsOfExperience"
+      />
+      <label for="6-10">6-10</label>
+      <input
+        type="radio"
+        id="10+"
+        value="10+"
+        v-model="formValues.yearsOfExperience"
+      />
+      <label for="10+">10+</label>
+    </div>
+
+    <div>
+      <button>Submit</button>
+    </div>
   </form>
 </template>
 
@@ -44,8 +116,17 @@ export default {
         profileSummary: "",
         country: "",
         jobLocation: [],
+        remoteWork: "no",
+        skillSet: [],
+        yearsOfExperience: "",
       },
     };
+  },
+  methods: {
+    submitForm(event) {
+      event.preventDefault();
+      console.log("Form values: ", this.formValues);
+    },
   },
 };
 </script>
