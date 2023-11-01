d092bff0df30f714db794c0a36573ce982ff3e03
Sanaz Bahmani <sanazbahmani35@gmail.com>
2023-11-01 15:29:21 +0330

    computed properties

--- src/App.vue
+++ src/App.vue
@@ -1,95 +1,62 @@
+<template>
+  <h2>Fullname: {{ firstName }} {{ lastName }}</h2>
+  <h2>Computed fullname: {{ fullName }}</h2>
+
+  <!-- <h2>
+    Total: {{ items.reduce((total, curr) => (total = total + curr.price), 0) }}
+  </h2> -->
+  <button
+    @click="
+      items.push({
+        id: 4,
+        title: 'Keyboard',
+        price: 50,
+      })
+    "
+  >
+    Add item
+  </button>
+  <h2>Computed total: {{ total }}</h2>
+</template>
+
 <script>
 export default {
   name: "App",
   data() {
     return {
-      greet: "Hello",
-      name: "Sanaz",
-      location: "<b>Helsinki, Finland</b>",
-      hack: `<a href='#' onclick='alert("You have been hacked!")'>Win a prize!</a>`,
-      headingId: "heading",
-      isDisabled: true,
-      status: "danger",
-      isPromoted: true,
-      isSoldout: true,
-      highlightColor: "orange",
-      headerSize: 50,
-      headerStyleObject: {
-        color: "orange",
-        fontSize: "50px",
-        padding: "20px",
-      },
-      baseStyleObject: {
-        fontSize: "50px",
-        padding: "10px",
-      },
-      successStyleObject: {
-        color: "green",
-        backgroundColor: "lightgreen",
-        border: "1px solid green",
-      },
-      dangerStyleObject: {
-        color: "darkred",
-        backgroundColor: "red",
-        border: "1px solid darkred",
-      },
+      firstName: "Sanaz",
+      lastName: "Bahmani",
+      items: [
+        {
+          id: 1,
+          title: "TV",
+          price: 100,
+        },
+        {
+          id: 2,
+          title: "Phone",
+          price: 200,
+        },
+        {
+          id: 3,
+          title: "Laptop",
+          price: 300,
+        },
+      ],
     };
+  },
+  computed: {
+    fullName() {
+      return `${this.firstName} ${this.lastName}`;
+    },
+    total() {
+      return this.items.reduce(
+        (total, curr) => (total = total + curr.price),
+        0
+      );
+    },
   },
 };
 </script>
 
-<template>
-  <p>{{ greet }} {{ name }}</p>
-  <div v-html="location"></div>
-  <div v-html="hack"></div>
-  <h2 :id="headingId">Heading</h2>
-  <button :disabled="isDisabled">Bind</button>
-  <h2 class="underline">Underlined Text</h2>
-  <h2 class="underline" :class="status">Status</h2>
-  <h2 :class="isPromoted && 'promoted'">Promoted Movie</h2>
-  <h2 :class="isSoldout ? 'sold-out' : 'new'">Soldout? movie</h2>
-  <h2 :class="['new', 'promoted']">Newly promoted movie</h2>
-  <h2 :class="[isPromoted && 'promoted', isSoldout ? 'sold-out' : 'new']">
-    Array conditional movie
-  </h2>
-  <h2
-    :class="{
-      promoted: isPromoted,
-      new: !isSoldout,
-      'sold-out': isSoldout,
-    }"
-  >
-    Object conditional movie
-  </h2>
-  <h2
-    :style="{
-      color: highlightColor,
-      fontSize: headerSize + 'px',
-      padding: '20px',
-    }"
-  >
-    Inline Style
-  </h2>
-  <h2 :style="headerStyleObject">Object style</h2>
-  <div :style="[baseStyleObject, successStyleObject]">Success Style</div>
-  <div :style="[baseStyleObject, successStyleObject, dangerStyleObject]">
-    Danger Style
-  </div>
-</template>
-
-<style scoped>
-.underline {
-  text-decoration: underline;
-}
-.promoted {
-  font-style: italic;
-}
-
-.new {
-  color: olivedrab;
-}
-
-.sold-out {
-  color: red;
-}
-</style>
+<style scoped></style>
