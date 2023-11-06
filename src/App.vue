b819e5308c9554f530169b203e7f7db994a0af53
Sanaz Bahmani <sanazbahmani35@gmail.com>
2023-11-06 10:44:08 +0330

    computed properties and v-for

--- src/App.vue
+++ src/App.vue
@@ -21,6 +21,13 @@
   <h2>Method Total: {{ getTotal() }}</h2>
 
   <input type="text" v-model="country" />
+
+  <template v-for="item in items" :key="item.id">
+    <h2 v-if="item.price > 100">{{ item.title }} - {{ item.price }}</h2>
+  </template>
+  <h2 v-for="item in expensiveItems" :key="item.id">
+    {{ item.title }} - {{ item.price }}
+  </h2>
 </template>
 
 <script>
@@ -69,6 +76,9 @@ export default {
         (total, curr) => (total = total + curr.price),
         0
       );
+    },
+    expensiveItems() {
+      return this.items.filter((item) => item.price > 100);
     },
   },
 };
