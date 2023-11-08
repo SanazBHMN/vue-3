c27579d33cb642b0d07e4b2d8e311dce84a488c3
Sanaz Bahmani <sanazbahmani35@gmail.com>
2023-11-08 15:05:08 +0330

    prop types and attributes

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,21 @@
 <template>
-  <Greet name="Sanaz" role="developer" />
+  <!-- <Greet name="Sanaz" role="developer" />
   <Greet name="Queena" role="talent acquisition" />
   <Greet name="Marina" role="marketing specialist" />
-  <Greet :name="name" :role="role" />
+  <Greet :name="name" :role="role" /> -->
+
+  <Article title="Article Title" :likes="5" :isPublished="true" />
 </template>
 
 <script>
 import Greet from "./components/Greet.vue";
+import Article from "./components/Article.vue";
+
 export default {
   name: "App",
   components: {
     Greet,
+    Article,
   },
   data() {
     return {
