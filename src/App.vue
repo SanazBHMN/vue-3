3adba663dd760789be14707e7ab326d2fb5271f0
Sanaz Bahmani <sanazbahmani35@gmail.com>
2023-11-12 15:25:34 +0330

    provide and inject

--- src/App.vue
+++ src/App.vue
@@ -10,22 +10,34 @@
     :likes="5"
     :isPublished="true"
   />
+
+  <br />
+
+  <h2>App component username: {{ name }}</h2>
+  <ComponentC />
 </template>
 
 <script>
 import Greet from "./components/Greet.vue";
 import Article from "./components/Article.vue";
+import ComponentC from "./components/ComponentC.vue";
 
 export default {
   name: "App",
   components: {
     Greet,
     Article,
+    ComponentC,
   },
   data() {
     return {
       name: "Fredrik",
       role: "CEO",
+    };
+  },
+  provide() {
+    return {
+      username: this.name,
     };
   },
 };
---
+++ src/components/ComponentC.vue
@@ -0,0 +1,17 @@
+<template>
+  <h2>component C</h2>
+  <componentE />
+</template>
+
+<script>
+import ComponentE from "./ComponentE.vue";
+
+export default {
+  name: "ComponentC",
+  components: {
+    ComponentE,
+  },
+};
+</script>
+
+<style scoped></style>
